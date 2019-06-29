e0844dadfdc867bb91f6f7c9e14e93723b9f9d5a
Clara Yip <clara.yip1@gmail.com>
2019-06-29 02:26:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,8 @@ def monopoly_with_third_tier
    :railroads=> 
    { 
      :pieces => 4, 
-     :names => {}
-     }, 
-     :rent_in_dollars => 
-     {
-     }
+     :names => {},
+     :rent_in_dollars => {
    }
 end
 
